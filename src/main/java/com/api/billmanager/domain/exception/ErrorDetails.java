d594bc21e20be3111e9cf4b34d6d6b3b6e8b3486
pedro.rosario <pdro.h.rosario@gmail.com>
2024-12-05 20:26:45 -0300

    Corrigir e adicionar tratamento de erro no GlobalExceptionHandler

--- src/main/java/com/api/billmanager/domain/exception/ErrorDetails.java
+++ src/main/java/com/api/billmanager/domain/exception/ErrorDetails.java
@@ -1,40 +1,19 @@
 package com.api.billmanager.domain.exception;
 
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
 import java.util.Date;
 
+@Data
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
 public class ErrorDetails {
     private Date timestamp;
     private String message;
     private String details;
-
-    public ErrorDetails(Date timestamp, String message, String details) {
-        this.timestamp = timestamp;
-        this.message = message;
-        this.details = details;
-    }
-
-    public Date getTimestamp() {
-        return timestamp;
-    }
-
-    public void setTimestamp(Date timestamp) {
-        this.timestamp = timestamp;
-    }
-
-    public String getMessage() {
-        return message;
-    }
-
-    public void setMessage(String message) {
-        this.message = message;
-    }
-
-    public String getDetails() {
-        return details;
-    }
-
-    public void setDetails(String details) {
-        this.details = details;
-    }
 
 }   
--- src/main/java/com/api/billmanager/infrastructure/exception/GlobalExceptionHandler.java
+++ src/main/java/com/api/billmanager/infrastructure/exception/GlobalExceptionHandler.java
@@ -4,8 +4,9 @@ import java.util.Date;
 
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
 import org.springframework.web.context.request.WebRequest;
 
 import com.api.billmanager.domain.exception.BillAlreadyPaidException;
@@ -14,10 +15,39 @@ import com.api.billmanager.domain.exception.CsvFailedImportException;
 import com.api.billmanager.domain.exception.CsvFileException;
 import com.api.billmanager.domain.exception.ErrorDetails;
 import com.api.billmanager.domain.exception.PaymentDateException;
+import com.api.billmanager.domain.exception.UserAlreadyExistException;
 import com.api.billmanager.domain.exception.UserNotFoundException;
 
-@ControllerAdvice
+@RestControllerAdvice
 public class GlobalExceptionHandler {
+
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    public ResponseEntity<ErrorDetails> handleValidationExceptions(MethodArgumentNotValidException ex,
+            WebRequest request) {
+
+        StringBuilder strBuilder = new StringBuilder();
+        ex.getBindingResult().getAllErrors().forEach((error) -> {
+            String message = error.getDefaultMessage();
+            strBuilder.append(String.format("%s", message));
+        });
+
+        ErrorDetails errorDetails = new ErrorDetails(
+                new Date(),
+                strBuilder.toString(),
+                request.getDescription(false));
+
+        return new ResponseEntity<>(errorDetails, HttpStatus.BAD_REQUEST);
+    }
+
+    @ExceptionHandler(UserAlreadyExistException.class)
+    public ResponseEntity<ErrorDetails> handleUserAlreadyExistException(UserAlreadyExistException ex, WebRequest request) {
+        ErrorDetails errorDetails = new ErrorDetails(
+                new Date(),
+                ex.getMessage(),
+                request.getDescription(false)
+        );
+        return new ResponseEntity<>(errorDetails, HttpStatus.BAD_REQUEST);
+    }
 
     @ExceptionHandler(UserNotFoundException.class)
     public ResponseEntity<ErrorDetails> handleUserNotFoundException(UserNotFoundException ex, WebRequest request) {
