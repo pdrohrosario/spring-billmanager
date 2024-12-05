d473d36164869bc0ea5a5c66a8633721c72ffb2b
pedro.rosario <pdro.h.rosario@gmail.com>
2024-12-05 19:54:26 -0300

    Adicionar conexão de banco, adicionar dependências do projeto e corrijir a migration de criação da tabela de Bills.

--- src/main/resources/db/migration/V2__create_table_bills.sql
+++ src/main/resources/db/migration/V2__create_table_bills.sql
@@ -1,6 +1,6 @@
 CREATE TABLE bills (
     id SERIAL PRIMARY KEY,
-    payment_date DATE NOT NULL
+    payment_date DATE NOT NULL,
     due_date DATE,
     amount NUMERIC(15, 2) NOT NULL,
     description VARCHAR(255) NOT NULL,
