ae9dca7b0db2bd2f406fb20fb80fb787b9c5f215
pedro.rosario <pdro.h.rosario@gmail.com>
2024-12-05 20:02:35 -0300

    Atualizar os dominios de User e Bill

--- src/main/java/com/api/billmanager/domain/model/User.java
+++ src/main/java/com/api/billmanager/domain/model/User.java
@@ -3,6 +3,10 @@ package com.api.billmanager.domain.model;
 import java.util.Collection;
 import java.util.List;
 
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
 import org.springframework.security.core.GrantedAuthority;
 import org.springframework.security.core.authority.SimpleGrantedAuthority;
 import org.springframework.security.core.userdetails.UserDetails;
@@ -15,8 +19,12 @@ import jakarta.persistence.Enumerated;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.OneToMany;
 import jakarta.persistence.Table;
 
+@Data
+@Builder
+@AllArgsConstructor
 @Entity
 @Table(name="users")
 public class User implements UserDetails{
@@ -31,6 +39,18 @@ public class User implements UserDetails{
 
     @Enumerated(EnumType.STRING)
     private Role role;
+
+    @OneToMany(mappedBy = "user")
+    private List<Bill> bills;
+
+    public User(){}
+
+    public User(String email, Long id, String password, Role role) {
+        this.email = email;
+        this.id = id;
+        this.password = password;
+        this.role = role;
+    }
 
     public User(String email, String password, Role role) {
         this.email = email;
@@ -51,26 +71,6 @@ public class User implements UserDetails{
     @Override
     public String getUsername() {
         return email;
-    }
-
-    @Override
-    public boolean isAccountNonExpired(){
-        return true;
-    }
-
-    @Override
-    public boolean isAccountNonLocked(){
-        return true;
-    }
-
-    @Override
-    public boolean isCredentialsNonExpired(){
-        return true;
-    }
-
-    @Override
-    public boolean isEnabled(){
-        return true;
     }
 
 }
