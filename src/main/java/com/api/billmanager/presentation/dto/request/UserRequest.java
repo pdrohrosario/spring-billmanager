85af691836e411efd6b5a482ea84f530a6875836
pedro.rosario <pdro.h.rosario@gmail.com>
2024-12-05 20:06:02 -0300

    Atualizar as classes de resquest e response para Bill, User e Pagination

--- src/main/java/com/api/billmanager/presentation/dto/request/UserRequest.java
+++ src/main/java/com/api/billmanager/presentation/dto/request/UserRequest.java
@@ -1,24 +1,20 @@
 package com.api.billmanager.presentation.dto.request;
 
+import com.api.billmanager.domain.model.User;
 import com.api.billmanager.presentation.dto.interfaces.Insert;
 
 import jakarta.validation.constraints.NotBlank;
-import jakarta.validation.constraints.NotNull;
+import lombok.*;
 
+@Data
+@Builder
+@AllArgsConstructor
 public class UserRequest {
 
-    @NotNull(groups = {Insert.class}, message = "The 'id' field is required")
-    private Long id;
-
-    @NotBlank(groups = {Insert.class}, message = "The 'email' field is required")
+    @NotBlank(groups = {Insert.class}, message = "The field 'email' is required")
     private String email;
 
-    public String getEmail() {
-        return email;
+    public User convertRequestToUser(){
+        return User.builder().email(this.email).build();
     }
-
-    public Long getId() {
-        return id;
-    }
-    
 }
